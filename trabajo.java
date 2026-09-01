c962689c9b9fcdea19608fa3ca682d1aecf377bd
juanmanu12-dot <juanmuzou@gmail.com>
2026-09-01 17:24:13 -0500

    se creo el constructor y sus parametros

--- trabajo.java
+++ trabajo.java
@@ -1,1 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
+
+public registro {
+
+    private string codigo;
+    private String nombre;
+    private String linea;
+    private int producidas;
+    private int defectuosas;
+    private double costoUnitario;
+    private int minutos;
+    private double kilos;
+    private int meta;
+
+
+    public registro(String codigo, String nombre, String linea, int producidas,
+    int defectuosas, double costoUnitario, int minutos,
+    double kilos, int meta){
+
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.linea = linea;
+        this.producidas = producidas;
+        this.defectuosas = defectuosas;
+        this.costoUnitario = costoUnitario;
+        this.minutos = minutos;
+        this.kilos = kilos;
+        this.meta = meta;
+
+
+    }
+
+
+
+}
+
+
+
+
+
+public class trabajo {
+
+
+
+}
